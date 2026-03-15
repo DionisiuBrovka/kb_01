b5d20465e7b40a02185cd9c6a5f990a2d6962ed3
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2026-03-15 19:43:39 +0300

    fix gaming layout

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -57,8 +57,8 @@ const uint16_t PROGMEM to_base_layer_combo[] =  {KC_LSFT, KC_SPC, KC_LCTL, KC_EN
 combo_t key_combos[] = {
     [COMBO_SUPER_LR] = COMBO(tg_super_layer_combo, SLTG),
     [COMBO_SUPER_LR_MOD] = COMBO(mo_super_layer_combo, SLMO), 
-    [COMBO_GAME_LR] = COMBO(tg_game_layer_combo, GLTG),
     [COMBO_PUNCT_LR_MOD] = COMBO(mo_punct_layer_combo, PLMO),
+    [COMBO_GAME_LR] = COMBO(tg_game_layer_combo, GLTG),
     [COMBO_CLEAN_LR] = COMBO(to_base_layer_combo, BTO),
 };
 
@@ -95,10 +95,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_GAME] = LAYOUT(
-         KC_ESC ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
-         KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
-         KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
-         KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+         KC_ESC ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_LBRC,
+         KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_RBRC,
+         KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
+         KC_LALT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_NUBS,
                                          _______,_______,_______,_______,
                                  _______,_______,_______,_______,_______,_______
     ),
