b697933721ad7c6846bb8aee755e6c2e8adf49b9
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2026-03-16 10:38:31 +0300

    add some changes

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,19 +1,24 @@
 #include QMK_KEYBOARD_H
 
-// TODO
-// - GAMEBAR KEY
-// - START RECORD GB KEY
-// - STOP RECORD GB KEY
-// - PUNCTUATIONS
-// - MEDIA KEYS
-
 // ---------------------------------------------------------------------------------------------------------
 
 #define SLTG TG(_SUPER)
 #define SLMO MO(_SUPER)
+#define PLTG TG(_PUNCT)
 #define PLMO MO(_PUNCT)
+#define MLTG TG(_MEDIA)
+#define MLMO MO(_MEDIA)
 #define GLTG TG(_GAME)
 #define BTO TO(_BASE)
+#define KC_STAB LSFT(KC_TAB)
+#define KC_CLN LSFT(KC_SCLN)
+#define KC_UOT LSFT(KC_QUOT)
+#define KC_LBRS LSFT(KC_LBRC)
+#define KC_RBRS LSFT(KC_RBRC)
+#define KC_LARW LSFT(KC_COMM)
+#define KC_RARW LSFT(KC_DOT)
+#define KC_LAR LSFT(KC_9)
+#define KC_RAR LSFT(KC_0)
 
 // ---------------------------------------------------------------------------------------------------------
 
@@ -21,26 +26,20 @@ enum layers {
     _BASE = 0,
     _SUPER,
     _PUNCT,
+    _MEDIA,
     _GAME,
-};
-
-enum custom_keycodes {
-    KC_STAB = LSFT(KC_TAB),
-    KC_ECUT  = LCTL(KC_X),
-    KC_ECPY = LCTL(KC_C),
-    KC_EPST  = LCTL(KC_V),
-    KC_CLN = LSFT(KC_SCLN),
-    KC_UOT = LSFT(KC_QUOT),
-    KC_LBRS = LSFT(KC_LBRC),
-    KC_RBRS = LSFT(KC_RBRC)
 };
 
 enum combos {
     COMBO_SUPER_LR,
     COMBO_SUPER_LR_MOD,
+    COMBO_SUPER_LR_MOD_ALT,
+    COMBO_PUNCT_LR,
     COMBO_PUNCT_LR_MOD,
+    COMBO_MEDIA_LR_MOD,
     COMBO_GAME_LR,
     COMBO_CLEAN_LR,
+    COMBO_CAPS,
     COMBO_LENGTH
 };
 
@@ -48,19 +47,28 @@ enum combos {
 
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
-const uint16_t PROGMEM tg_super_layer_combo[] = {KC_LSFT, KC_DEL, COMBO_END};
-const uint16_t PROGMEM mo_super_layer_combo[] = {KC_SPC, KC_LALT, COMBO_END};
-const uint16_t PROGMEM mo_punct_layer_combo[] =  {KC_LCTL, KC_LGUI, COMBO_END};
-const uint16_t PROGMEM tg_game_layer_combo[] =  {KC_LSFT, KC_SPC, KC_BSPC, KC_DEL, COMBO_END};
-const uint16_t PROGMEM to_base_layer_combo[] =  {KC_LSFT, KC_SPC, KC_LCTL, KC_ENT, KC_BSPC, KC_DEL, COMBO_END};
+const uint16_t PROGMEM tg_super_layer_combo[] =     {KC_LSFT, KC_DEL, COMBO_END};
+const uint16_t PROGMEM mo_super_layer_combo[] =     {KC_SPC, KC_LALT, COMBO_END};
+const uint16_t PROGMEM mo_super_layer_combo_alt[] = {KC_BSPC, KC_TAB, COMBO_END};
+const uint16_t PROGMEM tg_punct_layer_combo[] =     {KC_SPC, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM mo_punct_layer_combo[] =     {KC_LCTL, KC_LGUI, COMBO_END};
+const uint16_t PROGMEM mo_media_layer_combo[] =     {KC_ENT, KC_STAB, COMBO_END};
+const uint16_t PROGMEM tg_game_layer_combo[] =      {KC_LSFT, KC_SPC, KC_BSPC, KC_DEL, COMBO_END};
+const uint16_t PROGMEM to_base_layer_combo[] =      {KC_LSFT, KC_SPC, KC_LCTL, KC_ENT, KC_BSPC, KC_DEL, COMBO_END};
+const uint16_t PROGMEM caps_combo[] =               {KC_LCTL, KC_ENT, COMBO_END};
+
 
 combo_t key_combos[] = {
     [COMBO_SUPER_LR] = COMBO(tg_super_layer_combo, SLTG),
     [COMBO_SUPER_LR_MOD] = COMBO(mo_super_layer_combo, SLMO), 
+    [COMBO_SUPER_LR_MOD_ALT] = COMBO(mo_super_layer_combo_alt, SLMO), 
+    [COMBO_PUNCT_LR] = COMBO(tg_punct_layer_combo, PLTG),
     [COMBO_PUNCT_LR_MOD] = COMBO(mo_punct_layer_combo, PLMO),
     [COMBO_GAME_LR] = COMBO(tg_game_layer_combo, GLTG),
     [COMBO_CLEAN_LR] = COMBO(to_base_layer_combo, BTO),
-};
+    [COMBO_MEDIA_LR_MOD] = COMBO(mo_media_layer_combo, MLMO),
+    [COMBO_CAPS] = COMBO(caps_combo, KC_CAPS),
+};       
 
 // ---------------------------------------------------------------------------------------------------------
 
@@ -78,9 +86,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SUPER] = LAYOUT(
          KC_SLEP,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
-         KC_NO  ,KC_NO  ,KC_NO  ,KC_MSTP,KC_MPLY,KC_NO  ,KC_PSCR,KC_SCRL,KC_PAUS,KC_INS ,KC_WHOM,KC_F12 , 
-         KC_NO  ,KC_NO  ,KC_MPRV,KC_VOLD,KC_VOLU,KC_MNXT,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_WBAK,KC_NO  ,
-         KC_NO  ,KC_NO  ,KC_SPC ,KC_LCTL,KC_LALT,KC_MUTE,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,KC_WFWD,KC_NO  ,
+         KC_PWR ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_PSCR,KC_SCRL,KC_PAUS,KC_INS ,KC_NO  ,KC_F12 , 
+         KC_WBAK,KC_NO  ,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_NO  ,KC_WHOM,
+         KC_NO  ,KC_NO  ,KC_LALT,KC_LCTL,KC_LSFT,KC_LGUI,KC_TAB ,KC_LSFT,KC_LCTL,KC_LALT,KC_NO  ,KC_NO  ,
                                          _______,_______,_______,_______,
                                  _______,_______,_______,_______,_______,_______
     ),
@@ -88,8 +96,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_PUNCT] = LAYOUT(
          KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
          KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_LBRC,KC_RBRC,KC_LBRS,KC_RBRS,KC_NO  ,KC_NO  ,
-         KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_COMM,KC_DOT ,KC_SCLN,KC_CLN ,KC_NO  ,KC_NO  ,
-         KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_QUOT,KC_UOT ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+         KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_COMM,KC_DOT ,KC_SCLN,KC_CLN ,KC_LAR ,KC_RAR ,
+         KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_QUOT,KC_UOT ,KC_LARW,KC_RARW,KC_NO  ,KC_NO  ,
+                                         _______,_______,_______,_______,
+                                 _______,_______,_______,_______,_______,_______
+    ),
+
+    [_MEDIA] = LAYOUT(
+         KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+         KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_MUTE,KC_NO  ,KC_NO  ,KC_MSTP,KC_MPLY,KC_NO  ,KC_NO  ,KC_NO  ,
+         KC_NO  ,KC_NO  ,KC_NO  ,KC_MPRV,KC_MNXT,KC_NO  ,KC_NO  ,KC_VOLD,KC_VOLU,KC_NO  ,KC_NO  ,KC_NO  ,
+         KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
                                          _______,_______,_______,_______,
                                  _______,_______,_______,_______,_______,_______
     ),
@@ -128,7 +145,10 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             rgblight_setrgb(255, 25, 255); 
             break;
 
-            
+        case _MEDIA:
+            rgblight_setrgb(25, 255, 255); 
+            break;
+     
         default:
             rgblight_setrgb(2, 2, 2); 
             break;
